cefaad48eff818fcb35b13435b0a63078afdb471
George <145108792+Latesh-Splendler@users.noreply.github.com>
2025-03-13 00:32:27 +0300

    Update about_me.py

--- views/about_me.py
+++ views/about_me.py
@@ -1,8 +1,11 @@
 import streamlit as st
 
+
+from forms.contact import contact_form
+
 @st.dialog("Contact Me")
 def show_contact_form():
-    st.text_input("First Name")
+    contact_form()
 
 col1, col2 = st.columns(2, gap="small", vertical_alignment="center")
 
