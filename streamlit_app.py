54fbf395240df27051073a2fdd24d43a26e21163
George <145108792+Latesh-Splendler@users.noreply.github.com>
2025-03-12 23:40:44 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,6 +17,10 @@ project_2_page = st.Page(
   icon=":material/smart_toy:",
 )
 
-pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
-
+pg = st.navigation(
+  {
+    "Info": [about_page],
+    "Projects": [project_1_page, project_2_page],
+  }
+)
 pg.run()
